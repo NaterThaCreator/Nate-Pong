02609f8e5ee720fe539ba08530d851575f78af59
Nathan Fields <fieldsnathanj@gmail.com>
2025-01-30 18:19:04 -0600

    yupdate 3

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ let computerPaddleY = paddleY;
 let ballX = canvas.width / 2, ballY = canvas.height / 2;
 let ballDX = 4, ballDY = 4;
 
+// Scores
+let playerScore = 0;
+let computerScore = 0;
+
 // Controls
 document.addEventListener("mousemove", movePaddle);
 
@@ -34,39 +38,69 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawScore() {
+    ctx.font = "24px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText(`Player: ${playerScore}`, canvas.width / 4, 30);
+    ctx.fillText(`Computer: ${computerScore}`, (3 * canvas.width) / 4, 30);
+}
+
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Draw paddles and ball
     drawPaddle(0, paddleY);
     drawPaddle(canvas.width - paddleWidth, computerPaddleY);
+    drawBall();
 
+    // Draw the scoreboard
+    drawScore();
+
+    // Move the ball
     ballX += ballDX;
     ballY += ballDY;
 
+    // Ball-wall collisions
     if (ballY + ballRadius > canvas.height || ballY - ballRadius < 0) {
         ballDY = -ballDY;
     }
 
-    if (ballX - ballRadius < paddleWidth &&
-        ballY > paddleY && ballY < paddleY + paddleHeight) {
+    // Ball-paddle collisions
+    if (
+        ballX - ballRadius < paddleWidth &&
+        ballY > paddleY &&
+        ballY < paddleY + paddleHeight
+    ) {
         ballDX = -ballDX;
     }
 
-    if (ballX + ballRadius > canvas.width - paddleWidth &&
-        ballY > computerPaddleY && ballY < computerPaddleY + paddleHeight) {
+    if (
+        ballX + ballRadius > canvas.width - paddleWidth &&
+        ballY > computerPaddleY &&
+        ballY < computerPaddleY + paddleHeight
+    ) {
         ballDX = -ballDX;
     }
 
+    // Computer AI paddle movement
     computerPaddleY += (ballY - (computerPaddleY + paddleHeight / 2)) * 0.1;
 
-    if (ballX - ballRadius < 0 || ballX + ballRadius > canvas.width) {
-        ballX = canvas.width / 2;
-        ballY = canvas.height / 2;
-        ballDX = -ballDX;
+    // Update scores if ball goes out of bounds
+    if (ballX - ballRadius < 0) {
+        computerScore++; // Computer scores a point
+        resetBall();
+    } else if (ballX + ballRadius > canvas.width) {
+        playerScore++; // Player scores a point
+        resetBall();
     }
 
-    drawBall();
     requestAnimationFrame(gameLoop);
+}
+
+function resetBall() {
+    ballX = canvas.width / 2;
+    ballY = canvas.height / 2;
+    ballDX = -ballDX; // Reverse direction
 }
 
 gameLoop();
